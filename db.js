ad832fbbe7d95d91dd6adf09cebd46361355ddb9
Preston Chaplin <preston@New-MacBook-Pro.fios-router.home>
2019-03-06 14:41:26 -0500

    omg deployer

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 // const conn = new Sequelize(process.env.DATABASE_URL);
-const conn = new Sequelize(process.env.DATABASE_URL, "", "", {
+const conn = new Sequelize(process.env.DATABASE_URL, "preston", "", {
   dialect: "postgres"
 });
 // const conn = new Sequelize(
